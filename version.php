e334a44d414b058efe81378c92c941b06897e533
Hans Georg Schaathun <georg@schaathun.net>
2023-08-16 10:15:09 +0200

    version number

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2023033102; // The current module version (Date: YYYYMMDDXX).
+$plugin->version = 2023033104; // The current module version (Date: YYYYMMDDXX).
 $plugin->requires = 2022041900; // Moodle 4.0 (or above).
 $plugin->cron = 0; // Period in seconds for cron to run.
 $plugin->component = 'mod_jazzquiz';
